ad8ae31cb1399fb33a11b9236c0e77af5e7af408
Gudster <g.fristedt@gmail.com>
2022-12-22 14:06:46 +0100

    scripts etc

---
+++ static/script.js
@@ -0,0 +1,15 @@
+// för varje recept som hämtas:
+    // recipeId ++ 1
+
+
+let recipeId = 0
+let cardId = ("id" + recipeId)
+
+// för varje recept som hämtas:
+    // skapa nytt kort
+    // sätt kortets id == cardId
+
+$(document).ready(function(){
+    // Här lägger vi rättens namn, det som hämtas från APIt
+    $(".card-title").text("ETT RECEPT!");
+  });
