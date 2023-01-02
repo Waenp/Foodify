a829472f38529439b879ee71a24a5c1974661d12
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-01-02 09:51:38 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -17,17 +17,18 @@ $(document).ready(function(){
 $(".card").attr("id","cardID")
 $(".card-text").text("text")
 
-//.importantRule { !important; }
 
+
+// En funktion som sätter random bakgrundsfärg på #mainBody var gång sidan laddas.
 $(document).ready(function(){
     let colors = [ "rgb(228, 163, 159)", "rgb(188, 217, 233)", "rgb(74, 213, 128)" ];
     let splicedColor = [];
     let randomNumber = Math.floor( Math.random() * colors.length );
-    console.log(colors.length)
-    splicedColor.length ? colors.push( splicedColor[ 0 ] ) : "NOP";
     let randomColor = colors[ randomNumber ];
+
+    splicedColor.length ? colors.push( splicedColor[ 0 ] ) : "NOP";
     splicedColor = colors.splice( randomNumber, 1 );
-    console.log(randomColor, randomNumber)
+
     $("#mainBody").attr('style', "backgound-color: blue !important", backgroundcolor = randomColor);
     $("#mainBody").attr('style', $("#mainBody").attr('style') + '; ' + 'background-color:' + randomColor + ' !important')
 });
