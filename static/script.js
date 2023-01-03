dd1518fa8ca58c16523a818d51f82f31d7358621
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-01-03 15:40:18 +0100

    Javascript + receptsida

--- static/script.js
+++ static/script.js
@@ -1,22 +1,3 @@
-// för varje recept som hämtas:
-    // recipeId ++ 1
-
-
-let recipeId = 0
-let cardId = ("id" + recipeId)
-
-// för varje recept som hämtas:
-    // skapa nytt kort
-    // sätt kortets id == cardId
-
-$(document).ready(function(){
-    // Här lägger vi rättens namn, det som hämtas från APIt
-    $(".card-title").text("ETT RECEPT!");
-  });
-
-$(".card").attr("id","cardID")
-$(".card-text").text("text")
-
 // En funktion som sätter random bakgrundsfärg på #mainBody var gång sidan laddas.
 $(document).ready(function(){
     let colors = [ "rgb(228, 163, 159)", "rgb(188, 217, 233)", "rgb(25, 230, 140)" ];
