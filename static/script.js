71ea1694507ed962375f8a7c49f5a527dea713b7
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-01-06 15:23:01 +0100

    hejhej

--- static/script.js
+++ static/script.js
@@ -11,3 +11,19 @@ $(document).ready(function(){
     $("#mainBody").attr('style', "backgound-color: blue !important", backgroundcolor = randomColor);
     $("#mainBody").attr('style', $("#mainBody").attr('style') + '; ' + 'background-color:' + randomColor + ' !important')
 });
+
+
+document.getElementById("myButton").onclick = function () {
+    app.get('/login', function(req, res) {
+
+        var state = generateRandomString(16);
+        var scope = 'user-read-private user-read-email';
+        res.redirect('https://accounts.spotify.com/authorize?' +
+            querystring.stringify({
+                response_type: 'code',
+                client_id: client_id,
+                scope: scope,
+                redirect_uri: redirect_uri,
+                state: state
+            }));
+        })};
