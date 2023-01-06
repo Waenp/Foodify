b08f22abba13544bb2daa5ae701d9a04239f0d93
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-01-06 20:02:26 +0100

    Js test

--- static/script.js
+++ static/script.js
@@ -1,3 +1,20 @@
+function postRecipe() {
+    return function() {
+      
+      let input = $('#searchQuery input[name=searchInput]').val();
+      console.log(input)
+      
+      $.ajax({
+        method: "POST",
+        url: 'http://unicorns.idioti.se',
+        data: JSON.stringify(data),
+        headers: {"Accept": "application/json"}
+      })
+      .done(function(result) {
+      });
+    }
+  }
+
 // En funktion som sätter random bakgrundsfärg på #mainBody var gång sidan laddas.
 $(document).ready(function(){
     let colors = [ "rgb(228, 163, 159)", "rgb(188, 217, 233)", "rgb(25, 230, 140)" ];
@@ -10,20 +27,6 @@ $(document).ready(function(){
 
     $("#mainBody").attr('style', "backgound-color: blue !important", backgroundcolor = randomColor);
     $("#mainBody").attr('style', $("#mainBody").attr('style') + '; ' + 'background-color:' + randomColor + ' !important')
+
+    $('#postRecipe').click(postRecipe());
 });
-
-
-document.getElementById("myButton").onclick = function () {
-    app.get('/login', function(req, res) {
-
-        var state = generateRandomString(16);
-        var scope = 'user-read-private user-read-email';
-        res.redirect('https://accounts.spotify.com/authorize?' +
-            querystring.stringify({
-                response_type: 'code',
-                client_id: client_id,
-                scope: scope,
-                redirect_uri: redirect_uri,
-                state: state
-            }));
-        })};
