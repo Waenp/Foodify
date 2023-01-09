50b971a04d899d252343050b49b4f3dd86a77353
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-01-09 01:28:26 +0100

    Dokumentändringar + docstrings

--- static/script.js
+++ static/script.js
@@ -1,11 +1,11 @@
 
 
-// En funktion som sätter random bakgrundsfärg på #mainBody var gång sidan laddas.
+// Function that randomizes the background-color of the page
 $(document).ready(function(){
-    let colors = [ "rgb(228, 163, 159)", "rgb(188, 217, 233)", "rgb(25, 230, 140)" ];
-    let splicedColor = [];
-    let randomNumber = Math.floor( Math.random() * colors.length );
-    let randomColor = colors[ randomNumber ];
+    let colors = [ "rgb(228, 163, 159)", "rgb(188, 217, 233)", "rgb(25, 230, 140)" ]; // Bestämmer vilka färger som ska vara aktuella
+    let splicedColor = []; //
+    let randomNumber = Math.floor( Math.random() * colors.length ); // 
+    let randomColor = colors[ randomNumber ]; // 
 
     splicedColor.length ? colors.push( splicedColor[ 0 ] ) : "NOP";
     splicedColor = colors.splice( randomNumber, 1 );
@@ -13,10 +13,10 @@ $(document).ready(function(){
     $("#mainBody").attr('style', "backgound-color: blue !important", backgroundcolor = randomColor);
     $("#mainBody").attr('style', $("#mainBody").attr('style') + '; ' + 'background-color:' + randomColor + ' !important')
 
-    var input = document.getElementById("searchInput");
+    let input = document.getElementById("searchInput");
 
     // Execute a function when the user presses a key on the keyboard
-    input.addEventListener("keypress", function(event) {
+    input.addEventListener("keyup", function(event) {
       // If the user presses the "Enter" key on the keyboard
       if (event.key === "Enter") {
         // Cancel the default action, if needed
@@ -35,7 +35,7 @@ $(document).ready(function(){
       // Gör om sökningen till en sträng som delas på mellanslag
       let array = query.split(" ")
       console.log(array)
-    
+      
       $.ajax({
         method: "POST",
         url: 'http://localhost:5007/recipes',
@@ -47,6 +47,5 @@ $(document).ready(function(){
     }};
 
     $("#postRecipe").click(searchRecipe());
-
 
 });
