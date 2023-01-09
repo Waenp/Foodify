5de784b8b84ea77c652183341f820006cee35492
Gudster <g.fristedt@gmail.com>
2023-01-09 13:45:53 +0100

    test cherry-pick, jlc to bmm

---
+++ java-backend/src/main/java/org/example/Test.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
+
+public class Test {
+    public static void main(String[] args) {
+        /*Javalin javalin1 = Javalin.create(config -> {})
+                .get("/", ctx ->{
+                    ctx.result("Hello World");
+                    ctx.status(HttpStatus.OK);
+                })
+                .start(5007);
+         */
+
+    }
+}
